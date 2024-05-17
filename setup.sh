d7bcd9cd8f67e55d6dabf5aa70886bb6db7a3a0b
Aishwarya U <91381783+AISHWARYAAU@users.noreply.github.com>
2024-05-17 22:43:16 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,14 @@
+Updates to keyboard shortcuts … On Thursday, August 1, 2024, Drive keyboard shortcuts will be updated to give you first-letters navigation.Learn more
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
